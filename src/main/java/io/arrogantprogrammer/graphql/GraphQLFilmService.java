83f005d2fc8e0ac5f6e676cff4787ca3a8f1de02
jeremyrdavis <jeremyrdavis@mac.com>
2023-12-08 10:35:03 +0100

    updated to Films and heroes

--- src/main/java/io/arrogantprogrammer/graphql/GraphQLFilmService.java
+++ src/main/java/io/arrogantprogrammer/graphql/GraphQLFilmService.java
@@ -1,6 +1,7 @@
 package io.arrogantprogrammer.graphql;
 
 import io.arrogantprogrammer.domain.Film;
+import io.arrogantprogrammer.domain.Hero;
 import io.smallrye.graphql.client.typesafe.api.GraphQLClientApi;
 
 import java.util.List;
@@ -10,4 +11,9 @@ public interface GraphQLFilmService {
 
     List<Film> allFilms();
 
+    List<Hero> heroesWithSurname();
+
+    List<Hero> allHeroes();
+
+    Hero createHero(Hero hero);
 }
